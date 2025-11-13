229eaa8970edba88fce786bca9f57b1bc728ca08
sallydang01jan <sally01january@gmail.com>
2025-11-13 18:26:51 +0700

    journally out now.

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ dotenv.config();
 // --- CORS setup ---
 app.use(
   cors({
-    origin: "https://journally-gules.vercel.app",
+    origin: "https://journally-rho.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
